2acfb7140b5b6a594f3e80f3aafd15950d115e20
Andrew <cerkin-3@yandex.ru>
2019-07-31 21:50:05 +0300

    add client-info

--- src/gfycat_api/core.clj
+++ src/gfycat_api/core.clj
@@ -1,6 +1,8 @@
 (ns gfycat-api.core
+  (:require [clojure.data.json :as json])
   (:gen-class))
-
+(def client-info (json/read-str (slurp ".client_info.json")
+                                :key-fn #(keyword (clojure.string/replace % #"_" "-"))))
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
