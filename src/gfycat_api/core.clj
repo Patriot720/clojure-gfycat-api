b8441602996e0ff11024834f6e9b7f4b40a37fe7
Andrew <cerkin-3@yandex.ru>
2019-07-31 23:32:24 +0300

    aquire token

--- src/gfycat_api/core.clj
+++ src/gfycat_api/core.clj
@@ -1,9 +1,20 @@
 (ns gfycat-api.core
-  (:require [clojure.data.json :as json])
+  (:require [clojure.data.json :as json]
+            [clj-http.client :as client])
   (:gen-class))
+(defn- clojure-stylify [key]
+  (keyword (clojure.string/replace key #"_" "-")))
 (def client-info (json/read-str (slurp ".client_info.json")
-                                :key-fn #(keyword (clojure.string/replace % #"_" "-"))))
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+                                :key-fn clojure-stylify))
+
+(defn get-token []
+  (try
+    (-> (client/get "https://api.gfycat.com/v1/oauth/token"
+                    {:content-type :json
+                     :accept "text/html"
+                     :query-params {"grant_type" "client_credentials"
+                                    "client_id" (:client-id client-info)
+                                    "client_secret" (:client-secret client-info)}})
+        :body
+        (json/read-str :key-fn clojure-stylify))
+    (catch Exception e  (prn (ex-data e)))))
--- test/gfycat_api/core_test.clj
+++ test/gfycat_api/core_test.clj
@@ -1,7 +1,6 @@
 (ns gfycat-api.core-test
-  (:require [clojure.test :refer :all]
-            [gfycat-api.core :refer :all]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [gfycat-api.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest get-token-test
+  (is (= (:expires-in (core/get-token)) 3600)))
