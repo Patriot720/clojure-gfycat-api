ccd24059b6a8685a1bfa325c0c1cc668714f5258
Andrew <cerkin-3@yandex.ru>
2019-08-01 15:55:17 +0300

    single gfycat search

--- src/gfycat_api/core.clj
+++ src/gfycat_api/core.clj
@@ -8,14 +8,26 @@
   (keyword (clojure.string/replace key #"_" "-")))
 (def client-info (json/read-str (slurp ".client_info.json")
                                 :key-fn clojure-stylify))
+(defn- gfycat-request [path query-params]
+  (-> (client/get (str "https://api.gfycat.com/v1/" path)
+                  {:content-type :json
+                   :accept "text/html"
+                   :query-params query-params})
+      :body
+      (json/read-str :key-fn clojure-stylify)))
 (defn get-token []
-  (let [token-chan (chan)] (go
-                             (>! token-chan (-> (client/get "https://api.gfycat.com/v1/oauth/token"
-                                                            {:content-type :json
-                                                             :accept "text/html"
-                                                             :query-params {"grant_type" "client_credentials"
-                                                                            "client_id" (:client-id client-info)
-                                                                            "client_secret" (:client-secret client-info)}})
-                                                :body
-                                                (json/read-str :key-fn clojure-stylify))))
-       token-chan))
+  (let [token-chan (chan)]
+    (go
+      (try
+        (>! token-chan (gfycat-request "oauth/token"
+                                       {"grant_type" "client_credentials"
+                                        "client_id" (:client-id client-info)
+                                        "client_secret" (:client-secret client-info)}))
+        (catch Exception e (prn (ex-data e)  (close! token-chan)))))
+    token-chan))
+
+(defn search
+  ([token query & [count cursor]]
+   (gfycat-request "gfycats/search" {"search_text" query
+                                     "count" count
+                                     "cursor" cursor})))
--- test/gfycat_api/core_test.clj
+++ test/gfycat_api/core_test.clj
@@ -3,5 +3,13 @@
             [gfycat-api.core :as core]
             [clojure.core.async :refer [go <! <!! >! >!! alt! alt!! chan]]))
 
-(deftest get-token-test
-  (is (= (:expires-in (<!! (core/get-token))) 3600)))
+#_(deftest get-token-test
+    (let [token (<!! (core/get-token))]
+      (is (not (nil? token)))
+      (is (= (:expires-in token) 3600))))
+(def token (:access-token (<!! (core/get-token))))
+(deftest search-test
+  (testing "should return one gfycat search result"
+    (let [search-result (core/search token "thanos power up" 1)]
+      (is (= search-result {}))))
+  (testing "searchnig with cursor"))
