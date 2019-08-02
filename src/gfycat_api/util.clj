56a3d6eb8c276168a469eb5380234e8f48a3559e
Andrew <cerkin-3@yandex.ru>
2019-08-02 16:02:21 +0300

    documented get-gfycats-with

--- src/gfycat_api/util.clj
+++ src/gfycat_api/util.clj
@@ -5,5 +5,55 @@
 
 (defn get-gfycats-with [keys search-result]
   "returns list of gfycats with selected parameters,
-   removes all other parameters"
+   removes all other parameters
+   possible params:
+     :title
+   gifs urls:
+     :gif100px
+     :max2mbgif
+     :gifurl
+     :mp4url
+     :webmurl
+     :max1mbgif
+  other:
+     :framerate
+     :gfynumber
+     :width
+     :gatekeeper
+     :mobileposterurl
+     :source
+     :likes
+     :curated
+     :createdate
+     :languagetext2
+     :nsfw
+     :extralemmas
+     :hasaudio
+     :max5mbgif
+     :languagecategories
+     :mp4size
+     :url
+     :mobileurl
+     :gfyid
+     :gfyname
+     :posterurl
+     :hastransparency
+     :webpurl
+     :username
+     :published
+     :rating
+     :height
+     :views
+     :description
+     :tags
+     :webmsize
+     :gfyslug
+     :miniurl
+     :numframes
+     :dislikes
+     :avgcolor
+     :miniposterurl
+     :thumb100posterurl
+     :domainwhitelist
+"
   (map #(select-keys % keys) (:gfycats search-result)))
