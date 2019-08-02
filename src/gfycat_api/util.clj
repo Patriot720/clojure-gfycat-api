ce2234751de5b479da93d95fe6ea580244f5a6c9
Andrew <cerkin-3@yandex.ru>
2019-08-02 15:37:37 +0300

    change get-token arguments

--- src/gfycat_api/util.clj
+++ src/gfycat_api/util.clj
@@ -1,5 +1,8 @@
 (ns gfycat-api.util)
 
+(defn clojure-stylify [key]
+  (keyword (clojure.string/replace key #"_" "-")))
+
 (defn get-gfycats-with [keys search-result]
   "returns list of gfycats with selected parameters,
    removes all other parameters"
--- test/gfycat_api/core_test.clj
+++ test/gfycat_api/core_test.clj
@@ -1,14 +1,20 @@
 (ns gfycat-api.core-test
   (:require [clojure.test :refer [deftest is testing]]
+        [clojure.data.json :as json]
             [gfycat-api.core :as core]
+[gfycat-api.util :as util]
             ))
 
-#_(deftest get-token-test
-    (let [token (core/get-token)]
+(def client-info (json/read-str (slurp ".client_info.json")
+                                :key-fn util/clojure-stylify))
+
+(deftest get-token-test
+    (let [token (core/get-token (:client-id client-info) (:client-secret client-info))]
       (is (not (nil? token)))
       (is (= (:expires-in token) 3600))))
-#_(def token (:access-token (core/get-token)))
-#_(deftest search-test
+
+(def token (:access-token (core/get-token (:client-id client-info) (:client-secret client-info))))
+(deftest search-test
     (testing "should return one gfycat search result"
       (let [search-result (core/search token "thanos power up" 1)]
         (is (= (:cursor search-result) ""))))
