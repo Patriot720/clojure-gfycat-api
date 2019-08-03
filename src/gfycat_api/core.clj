fbec105dccc4a735d1f1f048f5fce5b2f4172c94
Andrew <cerkin-3@yandex.ru>
2019-08-03 16:00:40 +0300

    realised token is not needed

--- src/gfycat_api/core.clj
+++ src/gfycat_api/core.clj
@@ -1,10 +1,8 @@
 (ns gfycat-api.core
   (:require [clojure.data.json :as json]
             [clj-http.client :as client]
-            [gfycat-api.util :as util]
-            )
+            [gfycat-api.util :as util])
   (:gen-class))
-
 
 (defn- gfycat-request [path query-params]
   (-> (client/get (str "https://api.gfycat.com/v1/" path)
@@ -15,15 +13,17 @@
       (json/read-str :key-fn util/clojure-stylify)))
 
 (defn get-token [client-id client-secret]
-      (try
-        (gfycat-request "oauth/token"
-                                       {"grant_type" "client_credentials"
-                                        "client_id" client-id
-                                        "client_secret" client-secret})
-        (catch Exception e (prn (ex-data e)  ))))
+  (if (and client-id client-secret)
+    (try
+      (gfycat-request "oauth/token"
+                      {"grant_type" "client_credentials"
+                       "client_id" client-id
+                       "client_secret" client-secret})
+      (catch Exception e (prn (ex-data e))))))
 
 (defn search
-  ([token query & [count cursor]]
+"doesnt require a token"
+  ([query & [count cursor token]]
    (gfycat-request "gfycats/search" {"search_text" query
                                      "count" count
                                      "cursor" cursor})))
